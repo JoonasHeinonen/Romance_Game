bb5d58629bdebd869b4fe6adcdb3b61f71dd996c
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-07-25 22:00:21 +0300

    First commit.
    Added all what was made with Unity thus far. Here shall I continue!

---
+++ Yet_Another_Shitty_Game/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject character;
+    public string characterName;
+    public string[] messages;
+    private Vector3 playerPosition;
+    private Vector3 characterPosition;
+    private string text;
+    bool triggered;
+    bool isTalkedTo = false;
+
+    public int missionBriefinglength;
+    public bool isMissionComplete = false;
+    public bool doesFollow;
+    public bool willFollow = false;
+
+    // Enable only if followable
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    public Transform groundCheckPoint;
+    public Vector3 respawnPoint;
+    private bool isTouchingGround;
+    private Animator characterAnimation;
+
+    public int i;
+
+    PlayerController pn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pn = GameObject.Find("Player").GetComponent<PlayerController>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        characterAnimation = GetComponent<Animator>();
+    }
+
+    public void setMissionComplete(int x)
+    {
+        isMissionComplete = true;
+        Debug.Log(characterName + "'s quest is complete! It is " + isMissionComplete +"!");
+        setI(x);
+    }
+
+    private void setI(int x)
+    {
+        this.i = x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doesFollow)
+        {
+            characterAnimation.SetBool("OnGround", isTouchingGround);
+            characterAnimation.SetFloat("Speed", Mathf.Abs(rigidBody.velocity.x));
+            Debug.Log("Is touching the ground: " + isTouchingGround);
+            isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+            movement = Input.GetAxis("Horizontal");
+        }
+        if (isMissionComplete)
+        {
+            i = missionBriefinglength + 1;
+        }
+        playerPosition = new Vector3(
+            player.transform.position.x,
+            transform.position.y,
+            transform.position.z
+        );
+        characterPosition = new Vector3(
+            character.transform.position.x,
+            transform.position.y,
+            transform.position.z
+        );
+        if (playerPosition.x < characterPosition.x)
+        {
+            character.transform.localScale = new Vector2(1f, 1f);
+        }
+        else if (playerPosition.x > characterPosition.x)
+        {
+            character.transform.localScale = new Vector2(-1f, 1f);
+        }
+        if (willFollow)
+        {
+            if (movement < 0)
+            {
+                if (playerPosition.x + 3 < characterPosition.x)
+                    rigidBody.velocity = new Vector2(movement * speed * 1.25f, rigidBody.velocity.y);
+            }
+            else if (movement > 0)
+            {
+                if (playerPosition.x - 3 > characterPosition.x)
+                    rigidBody.velocity = new Vector2(movement * speed * 1.25f, rigidBody.velocity.y);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            triggered = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        var position = Camera.main.WorldToScreenPoint(character.transform.position);
+        var textSize = GUI.skin.label.CalcSize(new GUIContent(text));
+        if (triggered) //The dialogue starts.
+        {
+            if (isTalkedTo == false)
+            {
+                text = "Press Enter to talk!";
+                GUI.contentColor = Color.black;
+                GUI.Label(new Rect(position.x - 55, position.y - 60, textSize.x, textSize.y), text);
+            }
+            else if (isTalkedTo == true)
+            {
+                if (Input.GetKeyUp("space"))
+                {
+                    if (i < missionBriefinglength - 1)
+                    {
+                        i = i + 1;
+                    }
+                }
+                text = characterName + ": " + messages[i];
+                GUI.contentColor = Color.black;
+                GUI.Label(new Rect(10, 400, textSize.x, textSize.y), text);
+            }
+            if (Input.GetKeyDown("return") && isTalkedTo == false)
+            {
+                isTalkedTo = true;
+            }
+            else if (Input.GetKeyDown("backspace") && isTalkedTo == true)
+            {
+                i = 0;
+                isTalkedTo = false;
+                if (doesFollow)
+                {
+                    willFollow = true;
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            triggered = false;
+        }
+    }
+}
---
+++ Yet_Another_Shitty_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+    private Animator playerAnimation;
+    public Vector3 respawnPoint;
+    public LevelManager gameLevelManager;
+
+    private int money = 0;
+    private bool isNearCharacter = false;
+    public bool isTalking;
+
+    // Kenttäkohtaiset asiat
+    private int succeededMissions = 0;
+    private bool isNearGoofy = false;
+    private bool talkedToGoofy = false;
+    private string text;
+    private string currentMission;
+    private bool isGoofyQuestCompleted = false;
+    private bool hasMotorOil = false;
+
+    CharacterController gn;
+
+    // Use this for initialization
+    void Start()
+    {
+        gn = GameObject.Find("Goofy").GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+        respawnPoint = transform.position;
+        gameLevelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGoofyQuestCompleted)
+        {
+            hasMotorOil = false;
+        }
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        movement = Input.GetAxis("Horizontal");
+        if (isTalking == false)
+        {
+            if (movement > 0f)
+            {
+                rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+                transform.localScale = new Vector2(1f, 1f);
+            }
+            else if (movement < 0f)
+            {
+                rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+                transform.localScale = new Vector2(-1f, 1f);
+            }
+            else
+            {
+                rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            }
+            if (Input.GetButtonDown("Jump") && isTouchingGround)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+            }
+            playerAnimation.SetFloat("Speed", Mathf.Abs(rigidBody.velocity.x));
+            playerAnimation.SetBool("OnGround", isTouchingGround);
+        }
+        if (Input.GetKeyDown("return") && isNearCharacter)
+        {
+            Debug.Log("Maxie is talking with his father!");
+            isTalking = true;
+        }
+        if (Input.GetKeyDown("backspace") && isTalking == true)
+        {
+            isTalking = false;
+            if (isNearGoofy)
+            {
+                talkedToGoofy = true;
+            }
+        }
+        if (isNearGoofy == true && talkedToGoofy == true && isGoofyQuestCompleted == false)
+        {
+            currentMission = "Get motor oil!";
+        }
+        Debug.Log("Is the player touching the ground: " + isTouchingGround);
+        if (hasMotorOil)
+        {
+            currentMission = "Return to Goofy!";
+        }
+        if (isNearGoofy == true && hasMotorOil == true)
+        {
+            hasMotorOil = false;
+            isGoofyQuestCompleted = true;
+            succeededMissions++;
+            money += 10;
+            currentMission = "";
+            gn.setMissionComplete(8);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "FallDetector")
+        {
+            gameLevelManager.Respawn();
+        }
+        if (other.tag == "Checkpoint")
+        {
+            respawnPoint = other.transform.position;
+        }
+        if (other.gameObject.tag == "Characters")
+        {
+            isNearCharacter = true;
+            Debug.Log("You are near " + other.gameObject.name + "!");
+        }
+        if (other.gameObject.name == "Goofy")
+        {
+            isNearGoofy = true;
+        }
+        if (other.gameObject.name == "Motor_oil")
+        {
+            hasMotorOil = true;
+            Debug.Log("Both methods are working!");
+        }
+        if (other.gameObject.tag == "Coin")
+        {
+            money++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Characters")
+        {
+            isNearCharacter = false;
+            Debug.Log(isNearCharacter);
+        }
+        if (other.gameObject.name == "Goofy")
+        {
+            isNearGoofy = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        text = "Current mission: " + currentMission;
+        var textSize = GUI.skin.label.CalcSize(new GUIContent(text));
+        GUI.contentColor = Color.black;
+        GUI.Label(new Rect(10, 10, textSize.x, textSize.y), text);
+        GUI.Label(new Rect(10, 26, 500, textSize.y), "Succeeded missions: " + succeededMissions);
+        GUI.Label(new Rect(10, 42, 500, textSize.y), "Money: " + money);
+    }
+}
